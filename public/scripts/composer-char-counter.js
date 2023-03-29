94b7d1447bed0b0f4b0b132caf34000dc0d531cb
angelareit <anreit01@email.com>
2023-03-29 00:13:11 +0000

    added tweet bar, header, and nav bar

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  console.log('READY SIR');
+
+  //let textArea = $("#tweet-text");
+
+
+  //register char input on #twee-text text area
+  $("#tweet-text").keyup(function() {
+    //get this object count
+    let textArea = $(this);
+    console.log(textArea.val(), textArea.val().length);
+    //get counter object
+    let counter = textArea.next().find('output');
+    //assign the value
+    const remainingChar = 140 - $(this).val().length;
+    counter.val(remainingChar);
+
+
+    counter.css('color', remainingChar < 0 ? 'red' : 'white');
+    /*  if (remainingChar < 0) {
+       counter.css("color", "red");
+     } else if (remainingChar >= 0) {
+       counter.css("color", "white");
+     } */
+
+  });
+
+  $("#btn").on('click', function() {
+    console.log('(){}', this); //The this keyword is a reference to the button
+  });
+
+  $("#btn").on('click', () => {
+    textArea.val();
+    console.log('=>', this); //The this keyword here refers to something else!
+  });
+
+});
+
